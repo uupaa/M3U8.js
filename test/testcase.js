9892f6b14f726a99f9d83498da3e6c8c782ab08b
uupaa <uupaa.js@gmail.com>
2016-06-03 19:25:31 +0900

    add M3U8.filter

--- test/testcase.js
+++ test/testcase.js
@@ -22,6 +22,12 @@ var test = new Test("M3U8", {
         testM3U8_parseIndexPlaylist,
         testM3U8_buildMasterPlaylist,
         testM3U8_buildIndexPlaylist,
+        testM3U8_filterMovieHead,
+        testM3U8_filterLiveHead,
+        testM3U8_filterMovieSequenceIsNotZero,
+        testM3U8_filterLiveSequenceIsNotZero,
+        testM3U8_filterMovieSequenceIsNotZeroWithIndex,
+        testM3U8_filterLiveSequenceIsNotZeroWithIndex,
     ]);
 
 // --- test cases ------------------------------------------
@@ -78,7 +84,7 @@ media_w1360442349_1461.ts\n\
 
     console.dir(obj);
 
-    if (obj["version"] === 3 && obj["type"] === "INDEX") {
+    if (obj["version"] === 3 && obj["type"] === "LIVE") {
         if (obj["cache"] === false) {
             if (obj["duration"] === 2) {
                 if (obj["sequence"] === 1459) {
@@ -146,6 +152,251 @@ media_w1360442349_1461.ts';
         test.done(miss());
     }
 }
+
+function testM3U8_filterMovieHead(test, pass, miss) {
+
+    var source = '#EXTM3U\n\
+#EXT-X-VERSION:3\n\
+#EXT-X-TARGETDURATION:2\n\
+#EXT-X-MEDIA-SEQUENCE:0\n\
+#EXTINF:2.000,\n\
+a.ts\n\
+#EXTINF:2.000,\n\
+b.ts\n\
+#EXTINF:2.000,\n\
+c.ts\n\
+#EXTINF:2.000,\n\
+d.ts\n\
+#EXT-X-ENDLIST';
+
+    var indexPlaylistObject = M3U8.parse(source);
+    var stream = M3U8.filter(indexPlaylistObject)["stream"];
+
+    if (indexPlaylistObject.type === "MOVIE" &&
+        stream.length === 4 &&
+        stream[0].index === 0 &&
+        stream[1].index === 1 &&
+        stream[2].index === 2 &&
+        stream[3].index === 3) {
+        if (stream[0].url === "a.ts" &&
+            stream[1].url === "b.ts" &&
+            stream[2].url === "c.ts" &&
+            stream[3].url === "d.ts") {
+            if (stream[0].duration === 2.0 &&
+                stream[1].duration === 2.0 &&
+                stream[2].duration === 2.0 &&
+                stream[3].duration === 2.0) {
+                test.done(pass());
+                return;
+            }
+        }
+    }
+    test.done(miss());
+}
+
+function testM3U8_filterLiveHead(test, pass, miss) {
+
+    var source = '#EXTM3U\n\
+#EXT-X-VERSION:3\n\
+#EXT-X-TARGETDURATION:2\n\
+#EXT-X-MEDIA-SEQUENCE:0\n\
+#EXTINF:2.000,\n\
+a.ts\n\
+#EXTINF:2.000,\n\
+b.ts\n\
+#EXTINF:2.000,\n\
+c.ts\n\
+#EXTINF:2.000,\n\
+d.ts';
+
+    var indexPlaylistObject = M3U8.parse(source);
+    var stream = M3U8.filter(indexPlaylistObject)["stream"];
+
+    if (indexPlaylistObject.type === "LIVE" &&
+        stream.length === 2 &&
+        stream[0].index === 2 &&
+        stream[1].index === 3) {
+        if (stream[0].url === "c.ts" &&
+            stream[1].url === "d.ts") {
+            if (stream[0].duration === 2.0 &&
+                stream[1].duration === 2.0) {
+                test.done(pass());
+                return;
+            }
+        }
+    }
+    test.done(miss());
+}
+
+function testM3U8_filterMovieSequenceIsNotZero(test, pass, miss) {
+    // #EXT-X-ENDLIST がある → movie
+    // #EXT-X-MEDIA-SEQUENCE が 100 (0 以外)
+    // movie を途中から再生している → このケースは通常だと考えづらいが stream に対して何もしない
+
+    var source = '#EXTM3U\n\
+#EXT-X-VERSION:3\n\
+#EXT-X-TARGETDURATION:2\n\
+#EXT-X-MEDIA-SEQUENCE:100\n\
+#EXTINF:2.000,\n\
+a.ts\n\
+#EXTINF:2.000,\n\
+b.ts\n\
+#EXTINF:2.000,\n\
+c.ts\n\
+#EXTINF:2.000,\n\
+d.ts\n\
+#EXT-X-ENDLIST';
+
+    var indexPlaylistObject = M3U8.parse(source);
+    var stream = M3U8.filter(indexPlaylistObject)["stream"];
+
+    if (indexPlaylistObject.type === "MOVIE" &&
+        stream.length === 4 &&
+        stream[0].index === 100 + 0 &&
+        stream[1].index === 100 + 1 &&
+        stream[2].index === 100 + 2 &&
+        stream[3].index === 100 + 3) {
+        if (stream[0].url === "a.ts" &&
+            stream[1].url === "b.ts" &&
+            stream[2].url === "c.ts" &&
+            stream[3].url === "d.ts") {
+            if (stream[0].duration === 2.0 &&
+                stream[1].duration === 2.0 &&
+                stream[2].duration === 2.0 &&
+                stream[3].duration === 2.0) {
+                test.done(pass());
+                return;
+            }
+        }
+    }
+    test.done(miss());
+}
+
+function testM3U8_filterLiveSequenceIsNotZero(test, pass, miss) {
+    // #EXT-X-ENDLIST がない → live
+    // #EXT-X-MEDIA-SEQUENCE が 100 (0 以外)
+    // live を途中から再生している → 遅延を最小化するため threshold(3.0) を元に頭出しする → a.ts と b.ts がカットされ c.ts から再生する
+
+    var source = '#EXTM3U\n\
+#EXT-X-VERSION:3\n\
+#EXT-X-TARGETDURATION:2\n\
+#EXT-X-MEDIA-SEQUENCE:100\n\
+#EXTINF:2.000,\n\
+a.ts\n\
+#EXTINF:2.000,\n\
+b.ts\n\
+#EXTINF:2.000,\n\
+c.ts\n\
+#EXTINF:2.000,\n\
+d.ts';
+
+    var indexPlaylistObject = M3U8.parse(source);
+    var currentIndex = 0;
+    var threshold = 3.0;
+    var stream = M3U8.filter(indexPlaylistObject, currentIndex, threshold)["stream"]; // a.ts と b.ts がカットされる
+
+    if (indexPlaylistObject.type === "LIVE" &&
+        stream.length === 2 &&
+        stream[0].index === 100 + 2 &&
+        stream[1].index === 100 + 3) {
+        if (stream[0].url === "c.ts" &&
+            stream[1].url === "d.ts") {
+            if (stream[0].duration === 2.0 &&
+                stream[1].duration === 2.0) {
+                test.done(pass());
+                return;
+            }
+        }
+    }
+    test.done(miss());
+}
+
+
+function testM3U8_filterMovieSequenceIsNotZeroWithIndex(test, pass, miss) {
+    // #EXT-X-ENDLIST がある → movie
+    // #EXT-X-MEDIA-SEQUENCE が 100 (0 以外)
+    // movie を途中から再生している + index(102) が指定されている → このケースは通常だと考えづらいが stream に対して何もしない
+
+    var source = '#EXTM3U\n\
+#EXT-X-VERSION:3\n\
+#EXT-X-TARGETDURATION:2\n\
+#EXT-X-MEDIA-SEQUENCE:100\n\
+#EXTINF:2.000,\n\
+a.ts\n\
+#EXTINF:2.000,\n\
+b.ts\n\
+#EXTINF:2.000,\n\
+c.ts\n\
+#EXTINF:2.000,\n\
+d.ts\n\
+#EXT-X-ENDLIST';
+
+    var indexPlaylistObject = M3U8.parse(source);
+    var threshold = 3.0;
+    var currentIndex = 102;
+    var stream = M3U8.filter(indexPlaylistObject, currentIndex, threshold)["stream"];
+
+    if (indexPlaylistObject.type === "MOVIE" &&
+        stream.length === 4 &&
+        stream[0].index === 100 + 0 &&
+        stream[1].index === 100 + 1 &&
+        stream[2].index === 100 + 2 &&
+        stream[3].index === 100 + 3) {
+        if (stream[0].url === "a.ts" &&
+            stream[1].url === "b.ts" &&
+            stream[2].url === "c.ts" &&
+            stream[3].url === "d.ts") {
+            if (stream[0].duration === 2.0 &&
+                stream[1].duration === 2.0 &&
+                stream[2].duration === 2.0 &&
+                stream[3].duration === 2.0) {
+                test.done(pass());
+                return;
+            }
+        }
+    }
+    test.done(miss());
+}
+
+function testM3U8_filterLiveSequenceIsNotZeroWithIndex(test, pass, miss) {
+    // #EXT-X-ENDLIST がない → live
+    // #EXT-X-MEDIA-SEQUENCE が 100 (0 以外)
+    // live を途中から再生している + 現在102まで再生済み(currentIndex = 101)
+    //  → 遅延を最小化するため threshold(3.0) を元に頭出しする
+    //  → a.ts(index:100), b.ts(index:101), c.ts(index.102) がカットされ d.ts から再生する
+    //     再生可能な ts の総量(duration) が threshold 以下になるが、この場合は妥当
+
+    var source = '#EXTM3U\n\
+#EXT-X-VERSION:3\n\
+#EXT-X-TARGETDURATION:2\n\
+#EXT-X-MEDIA-SEQUENCE:100\n\
+#EXTINF:2.000,\n\
+a.ts\n\
+#EXTINF:2.000,\n\
+b.ts\n\
+#EXTINF:2.000,\n\
+c.ts\n\
+#EXTINF:2.000,\n\
+d.ts';
+
+    var indexPlaylistObject = M3U8.parse(source);
+    var threshold = 3.0;
+    var currentIndex = 102;
+    var stream = M3U8.filter(indexPlaylistObject, currentIndex, threshold)["stream"]; // a.ts, b.ts, c.ts がカットされる
+
+    if (indexPlaylistObject.type === "LIVE" &&
+        stream.length === 1 &&
+        stream[0].index === 100 + 3) {
+        if (stream[0].url === "d.ts") {
+            if (stream[0].duration === 2.0) {
+                test.done(pass());
+                return;
+            }
+        }
+    }
+    test.done(miss());
+}
+
 
 return test.run();
 
